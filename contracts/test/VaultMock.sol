f3c0b0b76b75c609c2f80dda2ac7c6914daf805f
William Griffiths <willjgriff@gmail.com>
2021-10-08 15:14:17 +0100

    Initial funds manager

---
+++ contracts/test/VaultMock.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.24;
+
+import "../ERC20.sol";
+
+contract VaultMock {
+
+    function transfer(address _token, address _to, uint256 _value) external {
+        require(_value > 0, "transfer zero value error");
+
+        if (_token == address(0)) {
+            // solium-disable-next-line security/no-send
+            require(_to.send(_value), "send reverted");
+        } else {
+            require(ERC20(_token).transfer(_to, _value), "token transfer reverted");
+        }
+    }
+
+    function balance(address _token) public view returns (uint256) {
+        if (_token == address(0)) {
+            return address(this).balance;
+        } else {
+            return ERC20(_token).balanceOf(address(this));
+        }
+    }
+}
